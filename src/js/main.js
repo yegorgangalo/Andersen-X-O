de934faf2034bc8f03003346a4064cc329d6ff51
Yegor Gangalo <gangaloyegor@gmail.com>
2021-03-15 17:29:18 +0200

    add logic for winner | for show Player & his suit

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,26 @@
-/* --------FORM in modal window------------- */
-  const formModalRef = document.querySelector('[data-form-modal]');
+const {PLAYER1, PLAYER2} = {
+    PLAYER1: {
+        name: "Player1",
+        suit: "",
+    },
+    PLAYER2: {
+        name: "Player2",
+        suit: "",
+    }
+}
 
+/* --------REFS------------- */
+const refs = {
+    formModalRef: document.querySelector('[data-form-modal]'),
+    backdropRef: document.querySelector("[data-backdrop]"),
+    spanPlayerNameRef: document.querySelector('[player-name]'),
+    spanPlayWithRef: document.querySelector('[play-with-xo]'),
+}
+
+const { formModalRef, backdropRef, spanPlayerNameRef, spanPlayWithRef } = refs;
+
+
+/* --------FORM in modal window------------- */
   const takeFormData = event => {
     event.preventDefault();
     const formRef = event.target;
@@ -11,17 +31,139 @@
       submittedData[key] = value;
     });
 
-    console.dir(submittedData);
+      if (submittedData.namePlayer1) {
+          PLAYER1.name = submittedData.namePlayer1;
+      }
+      if (submittedData.namePlayer1) {
+          PLAYER2.name = submittedData.namePlayer2;
+      }
+      PLAYER1.suit = submittedData.choosePlayWith;
+      PLAYER2.suit = PLAYER1.suit === "X" ? "O" : "X";
+
+      spanPlayerNameRef.textContent = PLAYER1.name;
+      spanPlayWithRef.textContent = PLAYER1.suit;
   };
 
-    formModalRef && formModalRef.addEventListener('submit', takeFormData);
-    /* ----------------------------------------- */
+formModalRef && formModalRef.addEventListener('submit', takeFormData);
 
-    const backdropRef =  document.querySelector("[data-backdrop]")
 
+    /* -------------------Modal Window---------------------- */
     function toggleModal() {
         backdropRef.classList.toggle("is-hidden");
     };
 
     formModalRef && formModalRef.addEventListener('submit', toggleModal);
-    /* ----------------------------------------- */
+/* ----------------------------------------- */
+
+const playCellCollection = document.querySelectorAll('.play-field td')
+playCellCollection.forEach(cell => cell.addEventListener('click', putMarkInCell));
+
+function putMarkInCell(event) {
+    const currentCell = event.target;
+    if (currentCell.textContent !== '') {
+        return;
+    }
+    currentCell.textContent = spanPlayerNameRef.textContent === PLAYER1.name
+        ? PLAYER1.suit
+        : PLAYER2.suit;
+
+    isWinner(currentCell);
+    changePlayer();
+}
+
+function changePlayer() {
+    spanPlayerNameRef.textContent === PLAYER1.name
+        ? (spanPlayerNameRef.textContent = PLAYER2.name, spanPlayWithRef.textContent = PLAYER2.suit)
+        : (spanPlayerNameRef.textContent = PLAYER1.name, spanPlayWithRef.textContent = PLAYER1.suit)
+}
+
+function isWinner(markedCell) {
+    const mark = markedCell.textContent;
+    // console.log('mark',mark);
+    switch (markedCell.id) {
+        case "1":
+            if (playCellCollection[1].textContent === mark && playCellCollection[2].textContent === mark ||
+                playCellCollection[3].textContent === mark && playCellCollection[6].textContent === mark ||
+                playCellCollection[4].textContent === mark && playCellCollection[8].textContent === mark
+            ) {
+                console.log('winner');
+                return true;
+                }
+            break;
+        case "2":
+            if (playCellCollection[0].textContent === mark && playCellCollection[2].textContent === mark ||
+                playCellCollection[4].textContent === mark && playCellCollection[7].textContent === mark
+            ) {
+                console.log('winner');
+                return true;
+                }
+            break;
+        case "3":
+            if (playCellCollection[0].textContent === mark && playCellCollection[1].textContent === mark ||
+                playCellCollection[5].textContent === mark && playCellCollection[8].textContent === mark ||
+                playCellCollection[4].textContent === mark && playCellCollection[6].textContent === mark
+            ) {
+                console.log('winner');
+                return true;
+                }
+            break;
+        case "4":
+            if (playCellCollection[0].textContent === mark && playCellCollection[6].textContent === mark ||
+                playCellCollection[4].textContent === mark && playCellCollection[5].textContent === mark
+            ) {
+                console.log('winner');
+                return true;
+                }
+            break;
+        case "5":
+            if (playCellCollection[0].textContent === mark && playCellCollection[8].textContent === mark ||
+                playCellCollection[1].textContent === mark && playCellCollection[7].textContent === mark ||
+                playCellCollection[2].textContent === mark && playCellCollection[6].textContent === mark ||
+                playCellCollection[3].textContent === mark && playCellCollection[5].textContent === mark
+            ) {
+                console.log('winner');
+                return true;
+                }
+            break;
+        case "6":
+            if (playCellCollection[2].textContent === mark && playCellCollection[8].textContent === mark ||
+                playCellCollection[3].textContent === mark && playCellCollection[4].textContent === mark
+            ) {
+                console.log('winner');
+                return true;
+                }
+            break;
+        case "7":
+            if (playCellCollection[0].textContent === mark && playCellCollection[3].textContent === mark ||
+                playCellCollection[2].textContent === mark && playCellCollection[4].textContent === mark ||
+                playCellCollection[7].textContent === mark && playCellCollection[8].textContent === mark
+            ) {
+                console.log('winner');
+                return true;
+                }
+            break;
+        case "8":
+            if (playCellCollection[1].textContent === mark && playCellCollection[4].textContent === mark ||
+                playCellCollection[6].textContent === mark && playCellCollection[8].textContent === mark
+            ) {
+                console.log('winner');
+                return true;
+                }
+            break;
+        case "9":
+            if (playCellCollection[0].textContent === mark && playCellCollection[4].textContent === mark ||
+                playCellCollection[2].textContent === mark && playCellCollection[5].textContent === mark ||
+                playCellCollection[6].textContent === mark && playCellCollection[7].textContent === mark
+            ) {
+                console.log('winner');
+                return true;
+                }
+            break;
+
+        default:
+            break;
+    }
+    // if ( markedCell.id = 1) {
+    //     return true;
+    // }
+}
